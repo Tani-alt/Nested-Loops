4f34f96d823241ef208a0848770b198d98853d7c
Tani-alt <Tanirika.gupta@gmail.com>
2025-02-06 21:38:48 +0530

    Character occurance

---
+++ Character occurence.py
@@ -0,0 +1,13 @@
+#nested loop
+string=input("Please enter a word :")
+char=input("Please enter a character :")
+i=0
+count=0
+while(i<len(string)) :
+    if (string[i]== char) :
+        count=count+1
+    i=i+1
+print("The total number of times",char,"has occured=",count)
+
+
+
